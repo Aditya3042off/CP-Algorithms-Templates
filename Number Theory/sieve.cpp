d73fe2aa01727d1ab5925cd8ebc52f4a2585af73
ADITYA SAI <84588725+Aditya3042off@users.noreply.github.com>
2025-05-01 12:27:13 +0530

    Updated sieve.cpp

--- Number Theory/sieve.cpp
+++ Number Theory/sieve.cpp
@@ -1,4 +1,4 @@
-// Time complexity -> O(Nlog(logN))
+// Complexity of sieve ->  O(N * Log(LogN)) 
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -18,26 +18,34 @@ const ll mod = 1e9 + 7,inf = 1e18;
 #define pb push_back
 #define fr(a,b) for(int i=0;i<b;i++)
 
-int N = 2e5;
-vb sieve(N+1);
+
+const int N = 2e5;
+// int sieve[N+1] -> max length can be 10^7
+bool sieve[N+1]; // max length can be 10^8 
 
 void computeSieve(int n = N) {
-	fr(0,n+1) sieve[i] = true;
+	for(int i=2;i<=n;i++) {
+		sieve[i] = true;
+	}
 
-	for(int i=2;i*i <=n;i++){
+	// Complexity of this loop is O(n*Log(Logn))
+	for(int i=2;i*i<=n;i++){
 		if(sieve[i] == false) continue;
+		
 		for(int j=i*i;j<=n;j+=i){
 			sieve[j] = false;
 		}
 	} 
 }
 
-vi getPrimes(int n) {
-	computeSieve(n);
-	vi primes;
 
-	fr(2,n+1) 
-		if(sieve[i]) primes.pb(i);
+vector<int> getPrimes(int n) {
+	computeSieve(n);
+	vector<int> primes;
+
+	for(int i=2;i<=n;i++) 
+		if(sieve[i] == true) primes.push_back(i);
+	
 	return primes;
 }
 
