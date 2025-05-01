4d568fe01ef1cac7ff3b3c656d0080cbbe964ff9
ADITYA SAI <84588725+Aditya3042off@users.noreply.github.com>
2025-05-01 16:49:16 +0530

    Updated segmentedSieve.cpp

--- Number Theory/segmentedSieve.cpp
+++ Number Theory/segmentedSieve.cpp
@@ -15,34 +15,43 @@ const ll mod = 1e9 + 7,inf = 1e18;
 #define pb push_back
 #define fr(a,b) for(int i=a;i<b;i++)
 
-int N = 2e5;
-vb sieve(N+1);
+const int N = 1e8;
+bool sieve[N+1];
 
 void computeSieve(int n = N) {
-	fr(2,n) sieve[i] = true;
+	for(int i=2;i<=n;i++){
+		sieve[i] = true;
+	}
 
 	for(int i=2;i*i<=n;i++){
-		if(sieve[i]){
+		if(sieve[i] == true){
 			for(int j=i*i;j<=n;j+=i)
 				sieve[j] = false;
 		}
 	}
 }
+computeSieve();
 
-vi getPrimes(int n) {
-	vi primes;
-	fr(2,n+1) 
-		if(sieve[i]) primes.pb(i);
+vector<int> generatePrimes(int n) {
+	vector<int> primes;
+	
+	for(int i=2;i<=n;i++){ 
+		if(sieve[i] == true) 
+			primes.push_back(i);
+	}
+	
 	return primes;
 }
 
-vb segmentedSieve(ll l,ll r){
-	vb dummy(r-l+1,true);
-	vi primes = getPrimes(sqrt(r));
+vector<bool> segmentedSieve(ll l,ll r){
+	vector<bool> dummy(r-l+1,true);
+	vector<int> primes = generatePrimes(sqrt(r));
 
 	for(auto pr : primes) {
-		ll start = max(pr*pr*1LL,(ll) ceil(l/(pr*1.0))*pr*1LL);
-		for(ll j = start;j<=r;j+=pr)
+		ll firstMultiple = (l/pr) * pr;
+		if(firstMultiple < l) firstMultiple += pr;
+		
+		for(ll j = max(firstMultiple,pr*pr); j<=r ; j+=pr)
 			dummy[j-l] = false;
 	}
 
